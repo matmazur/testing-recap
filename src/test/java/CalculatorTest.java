80498d0f9ac0a8c22f276d2a470eb4990279bfa7
Bingo Bobo <matmazur90@gmail.com>
2018-11-28 11:39:07 +0100

    added 2 more tests to check if class is null or not

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -4,18 +4,37 @@ import org.junit.jupiter.api.Test;
 public class CalculatorTest {
 
     @Test
-    public void shouldReturnMultiplicationResult(){
+    public void shouldReturnMultiplicationResult() {
 
         MyCalculator calculator = new MyCalculator();
 
-        Assertions.assertEquals(9.0,calculator.multiply(3,3),"jesus christ, this should return 9!");
+        Assertions.assertEquals(9.0, calculator.multiply(3, 3), "jesus christ, this should return 9!");
     }
 
     @Test
-    public void shouldReturnAllNumbersSummed(){
+    public void shouldReturnAllNumbersSummed() {
 
         MyCalculator calculator = new MyCalculator();
 
-        Assertions.assertEquals(17,calculator.add(3,3,4,7),"jesus christ, this should return 17!");
+        Assertions.assertEquals(17, calculator.add(3, 3, 4, 7), "jesus christ, this should return 17!");
+    }
+
+
+    @Test
+    public void shouldThrowNullPointerException() {
+
+        MyCalculator calculator = null;
+
+        Assertions.assertThrows(NullPointerException.class, () -> calculator.add(5, 3));
+
+    }
+
+    @Test
+    public void shouldNotThrowNullPointerException() {
+
+        MyCalculator calculator = new MyCalculator();
+
+        Assertions.assertAll(() -> calculator.add(5, 4));
+
     }
 }
