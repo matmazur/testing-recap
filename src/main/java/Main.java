d93603f93c12ea339f0a48185c9f7563811a6b2f
Bingo Bobo <matmazur90@gmail.com>
2018-11-29 08:44:09 +0100

    added surfire plugin for tests

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+        MyCalculator myCalculator = new MyCalculator();
+
+        System.out.println(myCalculator.add(5,4,3));
+    }
+}
